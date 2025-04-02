ba5a4737645a9fd2ae54803404e7da32d341570c
Aniket8699 <157409504+Aniket8699@users.noreply.github.com>
2025-04-02 15:12:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+app.get('/about', (req, res) => {
+    res.send('<h2>My About Page</h2>')
+  })
 
-app.listen(3000,function(){
+app.listen(8080,function(){
     console.log("Server Runniong at 3K")
 })
