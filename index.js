88b8989d321a9de0c3ffda1f33f1f757475b1805
Aniket8699 <157409504+Aniket8699@users.noreply.github.com>
2025-04-02 15:23:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 
 const app = express()
-app.use("/",express.static('/htmlweb'))
+app.use("/",express.static('./htmlweb'))
+app.use("/web",express.static('htmlweb/'))
 app.get('/about', (req, res) => {
     res.send('<h2>My About Page</h2>')
   })
